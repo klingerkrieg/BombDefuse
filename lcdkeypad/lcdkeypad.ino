3594f35f9aace8fdc7c0e5834524066ff6353252
Alan Klinger <klingerkrieg@gmail.com>
2022-11-11 14:38:04 -0300

    Sem delay

--- lcdkeypad/lcdkeypad.ino
+++ lcdkeypad/lcdkeypad.ino
@@ -1,11 +1,14 @@
 #include <LiquidCrystal.h>
+#include <Keypad.h>
+
 
 
 //Altere a sequencia para mudar a ordem de retirada dos fios
 //Os valores são as portas
-int  sequenciaFios[] = {51, 47, 43, 39};
-//int  sequenciaFios[] = {39, 47, 51, 43};
+int  sequenciaFios[] = {52, 51, 50, 49};
 const int qtdFios = 4;
+//Penalidade fio errado
+int penalidadeMin = 3;
 
 //Eses vetor guardará a sequencia que foi realizada
 //Ele irá controlar quando o fio for desconectado
@@ -16,8 +19,27 @@ bool seqRealizada[qtdFios]  = {};
 int seqPontuada[qtdFios]  = {};
 
 
-//Penalidade fio errado
-int penalidadeMin = 3;
+//momento em que a bomba iniciou a contagem
+int startMillis = 0;
+
+
+
+
+/////////////////////////KEYPAD
+const byte ROWS = 4; // Define the number of rows on the keypad
+const byte COLS = 3; // Define the number of columns on the keypad
+char keys[ROWS][COLS] = { // Matrix defining character to return for each key
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {24, 34, 32, 28}; //connect to the row pins (R0-R3) of the keypad
+byte colPins[COLS] = {26, 22, 30}; //connect to the column pins (C0-C2) of the keypad
+//initialize an instance of class
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+////////////////////////////////
+
 
 
  
@@ -26,7 +48,7 @@ const int rs = 8, en = 9, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 const int backLight = 10;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-const int buzzerPIN = 53;
+const int buzzerPIN = 14;
  
 // Limites para detecção das teclas, em ordem crescente
 struct {
@@ -53,7 +75,8 @@ void setup() {
     seqPontuada[i]  = 0;
     pinMode(sequenciaFios[i],INPUT);  
   }
-  
+
+
   //Progama o pino de backlight como saída
   pinMode(backLight, OUTPUT);
   pinMode(buzzerPIN,OUTPUT);
@@ -94,6 +117,10 @@ void gerenciaFiosConectados(){
             if (proxFio != i){
                 //Se tiver sido o fio errado
                 min_ -= penalidadeMin;
+                //apita 3 vezes para indicar que errou o fio
+                /*beep(100,50);
+                beep(100,50);
+                beep(100,50);*/
                 seqPontuada[i] = -1;
             } else {
                 //Se foi o fio certo
@@ -117,12 +144,121 @@ void gerenciaFiosConectados(){
 
 
 
-void beep(int beepOn, int beepOff){
-  tone(buzzerPIN, 10);
-  delay(beepOn);
-  noTone(buzzerPIN);
-  delay(beepOff);
+void gerenciaTeclado4x3(){
+  char key = keypad.getKey();
+  if (key){     // If key is pressed, send it to the Serial Monitor Window
+    Serial.println(key);
+  }  
 }
+
+
+
+/*
+
+//beep
+int beepStopMillis = 0;
+bool ligaBeep = true;
+int tempoBeep = 50;
+//timer
+int lastMilliSecond = 0;
+
+//lastMilliSecond     1500
+//millis_             2000
+//actualSecond        2
+//beepStopMillis      1050
+//lastSecond          1
+
+void beep(){
+  int millis_ = millis();           
+  int actualSecond = millis_/1000;
+  int lastSecond = lastMilliSecond/1000;
+  
+  //A cada segundo entra nesse if
+  if (lastSecond != actualSecond){
+      tone(buzzerPIN, 100);
+      beepStopMillis = millis_ + 50;
+  } else
+  if (beepStopMillis <= millis_){
+      noTone(buzzerPIN);  
+  }
+}
+
+
+void gerenciarTimer(){
+
+    int millis_ = millis();           
+    int actualSecond = millis_/1000;
+    int lastSecond = lastMilliSecond/1000;
+    
+    //A cada segundo entra nesse if
+    if (lastSecond != actualSecond){
+        lastMilliSecond = millis_;
+
+        //Quando seg chegar em -1 volta para 59
+        seg_--;
+        if( seg_ == -1){
+            seg_ = 59;
+            min_--;
+        }
+    
+        // Quando _min for menor que 0 a bomba explode
+        if( min_ < 0){
+            lcd.setCursor(0,1);
+            lcd.print("xxx");
+            ativada = false;
+        }  
+    }
+} */
+
+
+//beep
+int beepStopMillis = 0;
+bool ligaBeep = true;
+int tempoBeep = 50;
+//timer
+int lastMilliSecond = 0;
+
+//lastMilliSecond     1500
+//millis_             2000
+//actualSecond        2
+//beepStopMillis      1050
+//lastSecond          1
+
+//em bez de dividir por 1000, somar mais 1000
+
+void beep(){
+  int millis_ = millis();           
+  int actualSecond = millis_/1000;
+  int lastSecond = lastMilliSecond/1000;
+  
+  //A cada segundo entra nesse if
+  if (lastSecond != actualSecond){
+      lastMilliSecond = millis_;
+
+      //beep
+      tone(buzzerPIN, 100);
+      beepStopMillis = millis_ + 50;
+      
+
+      //Quando seg chegar em -1 volta para 59
+      seg_--;
+      if( seg_ == -1){
+          seg_ = 59;
+          min_--;
+      }
+  
+      // Quando _min for menor que 0 a bomba explode
+      if( min_ < 0){
+          lcd.setCursor(0,1);
+          lcd.print("xxx");
+          ativada = false;
+      }  
+  } else
+  if (beepStopMillis <= millis_){
+      noTone(buzzerPIN);  
+  }
+}
+
 
  
 // Laço principal
@@ -134,8 +270,14 @@ void loop() {
 
   //Quando ativada começará a contar
   if (ativada){
+
+      //registro o tempo onde a bomba realmente começou a contar
+      if (startMillis == 0){
+          startMillis = millis();  
+      }
   
       gerenciaFiosConectados();
+      //gerenciaTeclado4x3();
       
       lcd.setCursor(0,0);
   
@@ -149,11 +291,25 @@ void loop() {
         lcd.print("0");
       }
       lcd.print(seg_);
-  
+      
+      
+      beep();
+      //gerenciarTimer();
+      
+
+      /*if (ligaBeep){
+          tone(buzzerPIN, 10);
+          if (toneStartedMillis == 0)
+              toneStartedMillis = millis();
+          if (toneStartedMillis+tempoBeep > millis()+startMillis){
+              ligaBeep = false;
+              noTone(buzzerPIN);
+          }
+      }*/
   
       
       //Enquanto estiver acima de 5 min, 1 bip por segundo
-      if (min_ >= 5) {
+      /*if (min_ >= 5) {
         beepOn = 50;
         beepOff = 950;
         beep(beepOn, beepOff);
@@ -169,22 +325,10 @@ void loop() {
         beep(beepOn, beepOff);
         beep(beepOn, beepOff);
         beep(beepOn, beepOff);
-      }
+      }*/
   
   
-      //Quando seg chegar em -1 volta para 59
-      seg_--;
-      if( seg_ == -1){
-          seg_ = 59;
-          min_--;
-      }
-  
-      // Quando _min for menor que 0 a bomba explode
-      if( min_ < 0){
-          lcd.setCursor(0,1);
-          lcd.print("xxx");
-          ativada = false;
-      }
+      
   }
 
 
@@ -194,8 +338,6 @@ void loop() {
   // Lê a tensão no pino A0
   int leitura = analogRead(A0);
 
-  Serial.println(leitura);
- 
   // Identifica a tecla apertada pela tensão lida
   int teclaNova;
   for (teclaNova = 0; ; teclaNova++) {
