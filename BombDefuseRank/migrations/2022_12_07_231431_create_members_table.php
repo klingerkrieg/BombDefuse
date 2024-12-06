dbd013785bda64c02e40ebfe33d16c0361deb930
Alan Klinger Sousa Alves <2314163@ifrn.local>
2024-12-06 09:27:13 -0300

    update

---
+++ BombDefuseRank/migrations/2022_12_07_231431_create_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('age')->nullable();
+            $table->string('course')->nullable();
+            $table->integer('participate')->nullable();
+            $table->integer('participateCount')->nullable();
+            $table->foreignId("team_id")->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
